74f6f444a41dbca54288067dd0483f39847ae34b
PavelSmirnoff <smirnov.pwd@gmail.com>
2020-03-22 17:47:47 +0300

    Индикация перегрева CPU

--- led-indication.sh
+++ led-indication.sh
@@ -8,12 +8,15 @@ declare -i br2=0;
 declare -i sec_now=0;
 declare -i msec_now=0;
 
+cpu_temp_ust=46;
+declare -i cpu_temp=0;
+
 #Включение синего светодиода при работе с диском:
 echo disk-activity > /sys/devices/platform/leds/leds/cubietruck:blue:usr/trigger
 
 
 #Вывод информации о состоянии батареи
-cat /sys/class/power_supply/axp20x-battery/uevent;
+#cat /sys/class/power_supply/axp20x-battery/uevent;
 
 while true; do
 
@@ -25,8 +28,8 @@ br2=sec_now%3;
 bat_compacity=$(cat /sys/class/power_supply/axp20x-battery/capacity);
 bat_status=$(cat /sys/class/power_supply/axp20x-battery/status);
 
-cat /sys/class/power_supply/axp20x-battery/capacity;
-cat /sys/class/power_supply/axp20x-battery/status;
+#cat /sys/class/power_supply/axp20x-battery/capacity;
+#cat /sys/class/power_supply/axp20x-battery/status;
 
 #Оранжевый индикатор разряда батареи
 	if [ "$bat_status" == "Charging" ]; then
@@ -43,7 +46,7 @@ cat /sys/class/power_supply/axp20x-battery/status;
 	if [[ $bat_compacity -lt $bat_min_ust ]]; then
         	echo $br > /sys/devices/platform/leds/leds/cubietruck:orange:usr/brightness;
         	if [[ $bat_compacity -le $bat_min_ust-10 ]] && [ "$bat_status" != "Charging" ]; then
-                	#shutdown -h now
+#                	shutdown -h now
                 	echo "Пора выключать кубик";
         	fi
 	fi
@@ -51,5 +54,21 @@ cat /sys/class/power_supply/axp20x-battery/status;
 	if [[ $bat_compacity -gt 98 ]]; then
 		echo 0 > /sys/devices/platform/leds/leds/cubietruck:orange:usr/brightness;
 	fi
+
+
+# Белый индикатор при повышении температуры CPU выше 46 градусов
+cpu_temp=10#$(cat /sys/devices/virtual/thermal/thermal_zone0/temp)/1000;
+	if [[ $cpu_temp -le $cpu_temp_ust ]]; then
+        	echo 0 > /sys/devices/platform/leds/leds/cubietruck:white:usr/brightness;
+#		echo 0;
+	elif [[ $cpu_temp -le $cpu_temp_ust+10 ]]; then
+		echo $br1 > /sys/devices/platform/leds/leds/cubietruck:white:usr/brightness;
+#		echo 1;
+	else
+        	echo $br1 > /sys/devices/platform/leds/leds/cubietruck:white:usr/brightness;
+#		echo 2;
+	fi
+echo "CPU $cpu_temp $cpu_temp_ust";
+
 done
 
